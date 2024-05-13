57dfdcb0d0129271797af71f06e6624200e186d7
SyedSibtainRazvi <syedsibtain191@gmail.com>
2024-05-13 23:40:38 +0530

    style: added images and customised testimonial

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import { Navbar } from "./components/Navbar";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
+import { Testimonial } from "./components/Testimonial";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Header />
+      <Testimonial />
       <Footer />
     </>
   );
